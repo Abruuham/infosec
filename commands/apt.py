f90399eb0254f864db7f063facf6362874478a34
Abraham Calvillo <31970808+Abruuham@users.noreply.github.com>
2021-12-01 17:53:58 -0800

    Adding ls and cat commands

--- commands/apt.py
+++ commands/apt.py
@@ -45,17 +45,17 @@ class APTCommand:
 
     def version(self):
         self.write(
-            """apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06
-            Supported modules:
-            *Ver: Standard .deb
-            *Pkg:  Debian dpkg interface (Priority 30)
-             Pkg:  Debian APT solver interface (Priority -1000)
-             S.L: 'deb' Standard Debian binary tree
-             S.L: 'deb-src' Standard Debian source tree
-             Idx: Debian Source Index
-             Idx: Debian Package Index
-             Idx: Debian Translation Index
-             Idx: Debian dpkg status file
+            """apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06\r\n
+            Supported modules:\r\n
+            *Ver: Standard .deb\r\n
+            *Pkg:  Debian dpkg interface (Priority 30)\r\n
+             Pkg:  Debian APT solver interface (Priority -1000)\r\n
+             S.L: 'deb' Standard Debian binary tree\r\n
+             S.L: 'deb-src' Standard Debian source tree\r\n
+             Idx: Debian Source Index\r\n
+             Idx: Debian Package Index\r\n
+             Idx: Debian Translation Index\r\n
+             Idx: Debian dpkg status file\r\n
              Idx: EDSP scenario file\r\n
              """
         )
@@ -63,48 +63,48 @@ class APTCommand:
 
     def help(self):
         self.write(
-            """apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06
-            Usage: apt-get [options] command
-                   apt-get [options] install|remove pkg1 [pkg2 ...]
-                   apt-get [options] source pkg1 [pkg2 ...]
+            """apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06\r\n
+            Usage: apt-get [options] command\r\n
+                   apt-get [options] install|remove pkg1 [pkg2 ...]\r\n
+                   apt-get [options] source pkg1 [pkg2 ...]\r\n
     
-            apt-get is a simple command line interface for downloading and
-            installing packages. The most frequently used commands are update
-            and install.
+            apt-get is a simple command line interface for downloading and\r\n
+            installing packages. The most frequently used commands are update\r\n
+            and install.\r\n
+    \r\n
+            Commands:\r\n
+               update - Retrieve new lists of packages\r\n
+               upgrade - Perform an upgrade\r\n
+               install - Install new packages (pkg is libc6 not libc6.deb)\r\n
+               remove - Remove packages\r\n
+               autoremove - Remove automatically all unused packages\r\n
+               purge - Remove packages and config files\r\n
+               source - Download source archives\r\n
+               build-dep - Configure build-dependencies for source packages\r\n
+               dist-upgrade - Distribution upgrade, see apt-get(8)\r\n
+               dselect-upgrade - Follow dselect selections\r\n
+               clean - Erase downloaded archive files\r\n
+               autoclean - Erase old downloaded archive files\r\n
+               check - Verify that there are no broken dependencies\r\n
+               changelog - Download and display the changelog for the given package\r\n
+               download - Download the binary package into the current directory\r\n
     
-            Commands:
-               update - Retrieve new lists of packages
-               upgrade - Perform an upgrade
-               install - Install new packages (pkg is libc6 not libc6.deb)
-               remove - Remove packages
-               autoremove - Remove automatically all unused packages
-               purge - Remove packages and config files
-               source - Download source archives
-               build-dep - Configure build-dependencies for source packages
-               dist-upgrade - Distribution upgrade, see apt-get(8)
-               dselect-upgrade - Follow dselect selections
-               clean - Erase downloaded archive files
-               autoclean - Erase old downloaded archive files
-               check - Verify that there are no broken dependencies
-               changelog - Download and display the changelog for the given package
-               download - Download the binary package into the current directory
-    
-            Options:
-              -h  This help text.
-              -q  Loggable output - no progress indicator
-              -qq No output except for errors
-              -d  Download only - do NOT install or unpack archives
-              -s  No-act. Perform ordering simulation
-              -y  Assume Yes to all queries and do not prompt
-              -f  Attempt to correct a system with broken dependencies in place
-              -m  Attempt to continue if archives are unlocatable
-              -u  Show a list of upgraded packages as well
-              -b  Build the source package after fetching it
-              -V  Show verbose version numbers
-              -c=? Read this configuration file
-              -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
-            See the apt-get(8), sources.list(5) and apt.conf(5) manual
-            pages for more information and options.
+            Options:\r\n
+              -h  This help text.\r\n
+              -q  Loggable output - no progress indicator\r\n
+              -qq No output except for errors\r\n
+              -d  Download only - do NOT install or unpack archives\r\n
+              -s  No-act. Perform ordering simulation\r\n
+              -y  Assume Yes to all queries and do not prompt\r\n
+              -f  Attempt to correct a system with broken dependencies in place\r\n
+              -m  Attempt to continue if archives are unlocatable\r\n
+              -u  Show a list of upgraded packages as well\r\n
+              -b  Build the source package after fetching it\r\n
+              -V  Show verbose version numbers\r\n
+              -c=? Read this configuration file\r\n
+              -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp\r\n
+            See the apt-get(8), sources.list(5) and apt.conf(5) manual\r\n
+            pages for more information and options.\r\n
                                    This APT has Super Cow Powers.\r\n"""
         )
         return
